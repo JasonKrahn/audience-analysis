db8dbf38df878442224543181726e0a6cf4b903f
Andrey Vykhodtsev <anvykhod@microsoft.com>
2017-10-09 01:19:44 +0200

    latest changes

--- app.py
+++ app.py
@@ -7,8 +7,7 @@ from flask import Flask, request, Response, jsonify
 import requests
 import logging
 import yaml
-from azure.servicebus import ServiceBusService
-import util
+import util as u
 
 import pickle
 from collections import deque
@@ -16,46 +15,42 @@ from collections import deque
 
 
 log = logging.getLogger()
-with open('config.yaml', 'r') as f: conf = yaml.load(f)
-cognitive_conf = conf['cognitive_services']
-event_hub_conf = conf['event_hubs']
 
-app = Flask(__name__, static_url_path='/static')
+app = Flask(__name__, static_url_path="/static")
 
 # Make the WSGI interface available at the top level so wfastcgi can get it.
 wsgi_app = app.wsgi_app
-sbs = ServiceBusService(event_hub_conf["namespace"], shared_access_key_name=event_hub_conf["policy_name"], shared_access_key_value=event_hub_conf["policy_secret"])
-
+sbs = u.get_ehub()
 # two in-memory structures to maintain last faceAttr and last JPEG
-sessionLastFaceAttr = deque(maxlen = conf['app']['max_sessions'])
+sessionLastFaceAttr = deque(maxlen = int(u.get_setting("app","max_sessions")))
 sessionLastJPEG = {}
 
-@app.route('/',methods=['GET', 'POST'])
+@app.route("/",methods=["GET", "POST"])
 def process_image():
 
-    url = 'https://' + cognitive_conf['faceapi_uri'] + '/face/v1.0/detect'
+    url = "https://" + u.get_setting("cognitive_services", "faceapi_uri") + "/face/v1.0/detect"
     #url = "https://hookb.in/vaP0en5Y"
     #url = "https://requestb.in/10yi8601"
-    subscription_key = cognitive_conf['subscription_key']
-    sess_id = request.headers['SESSIONID']
+    subscription_key = u.get_setting("cognitive_services","subscription_key")
+    sess_id = request.headers["SESSIONID"]
 
-    if request.headers['Content-Type'] == 'application/octet-stream':
-        headers = {'Content-Type': 'application/octet-stream',
-                    'Ocp-Apim-Subscription-Key': subscription_key}
+    if request.headers["Content-Type"] == "application/octet-stream":
+        headers = {"Content-Type": "application/octet-stream",
+                    "Ocp-Apim-Subscription-Key": subscription_key}
 
         # Request parameters.
         params = {
-            'returnFaceId': 'true',
-            'returnFaceLandmarks': 'false',
-            'returnFaceAttributes': 'age,gender,smile,emotion',
+            "returnFaceId": "true",
+            "returnFaceLandmarks": "false",
+            "returnFaceAttributes": "age,gender,smile,emotion",
         }
 
         r = requests.post(url, params = params, headers = headers, data = request.data)
 
-        ret = util.get_agg_face_attrs(r.json())
-        ret['session_id'] = sess_id
+        ret = u.get_agg_face_attrs(r.json())
+        ret["session_id"] = sess_id
 
-        sbs.send_event(event_hub_conf['hub_name'], json.dumps(ret))
+        sbs.send_event(u.get_setting("event_hubs","hub_name"), json.dumps(ret))
 
         #save file for testing purposes
         #with open("test_json.pkl", "w") as f:
@@ -63,39 +58,39 @@ def process_image():
 
         #Updating the session metadata and last JPEG
         resp = {}
-        resp['SESSIONID'] = sess_id
-        resp['faceAttributes'] = r.json()
+        resp["SESSIONID"] = sess_id
+        resp["faceAttributes"] = r.json()
         sessionLastFaceAttr.appendleft(resp)
         sessionLastJPEG[sess_id] = request.data
 
         log.debug(json.dumps(ret))
 
-        return Response(json.dumps({"message":"Success"}), status=200, mimetype='application/json')
+        return Response(json.dumps({"message":"Success"}), status=200, mimetype="application/json")
 
     else:
-        return util.bad_message("Wrong content type, only octet-stream is supported")
+        return u.bad_message("Wrong content type, only octet-stream is supported")
 
-@app.route('/sessions',methods=['GET'])
+@app.route("/sessions",methods=["GET"])
 def get_sessions():
     resp = jsonify({"sessions":list(sessionLastFaceAttr)})
     resp.status_code = 200
     return resp
 
-@app.route('/session-jpeg/<session_id>',methods=['GET'])
+@app.route("/session-jpeg/<session_id>",methods=["GET"])
 def get_session_jpeg(session_id):
     if session_id in sessionLastJPEG:
         return Response(sessionLastJPEG[session_id], status=200, mimetype="application/octet-stream")
     else:
-        return util.bad_message("no such session")
+        return u.bad_message("no such session")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     log.setLevel(logging.DEBUG)
     log.addHandler(logging.StreamHandler(stream = sys.stdout))
 
-    HOST = os.environ.get('SERVER_HOST', '0.0.0.0')
+    HOST = os.environ.get("SERVER_HOST", "0.0.0.0")
     try:
-        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+        PORT = int(os.environ.get("SERVER_PORT", "5555"))
     except ValueError:
         PORT = 5555
     app.run(HOST, PORT)
--- util.py
+++ util.py
@@ -2,9 +2,40 @@ import json
 import logging
 import collections
 from flask import jsonify
+import yaml, os, sys, subprocess
+from azure.servicebus import ServiceBusService
+from itertools import chain
 
 log = logging.getLogger()
+try:
+    with open('config.yaml', 'r') as f:
+        conf = yaml.load(f)
+except FileNotFoundError:
+    log.warning("Config file not fould, going with env vars")
 
+def get_setting(category, setting_name):
+    '''
+    returns a setting either from config.yaml of from local environment.\
+    When retrieving form local environment the category and variable names transformed\
+    to upper case and joined together. E.g COGNITIVE_SERVICES_SUBSCRIPTION_KEY
+    '''
+    env_var_name = category.upper() + '_' + setting_name.upper()
+    cat, nm = category.lower() , setting_name.lower()
+
+    if env_var_name in os.environ:
+        return os.environ[env_var_name]
+    elif cat in conf and nm in conf[cat]:
+        return conf[cat][nm]
+    else:
+        raise ValueError("No setting found - category: {} name: {}, or env var {}"
+                            .format(cat, nm, env_var_name))
+
+def get_ehub():
+    ehub_nspace = get_setting("event_hubs","namespace")
+    ehub_policy = get_setting("event_hubs","policy_name")
+    ehub_key = get_setting("event_hubs","policy_secret")
+    sbs = ServiceBusService(ehub_nspace, shared_access_key_name=ehub_policy, shared_access_key_value=ehub_key)
+    return sbs
 
 def get_agg_face_attrs(js):
     '''
@@ -45,15 +76,118 @@ def bad_message(msg):
     resp.status_code = 400
     return resp
 
-if __name__ == "__main__":
-    import yaml, sys, pickle
-    with open('config.yaml', 'r') as f: conf = yaml.load(f)
-    log.setLevel(logging.getLevelName(conf['app']['log_level']))
-    log.addHandler(logging.StreamHandler(stream = sys.stdout))
+def set_env(exclude_cat = ['web_app'], return_values = False):
+    '''
+    Sets env vars from config. Returns list of set environment variables
+    '''
+    vlist = []
+    for i in conf.keys():
+        if i not in exclude_cat:
+            for j in conf[i].keys():
+                env_var_name = i.upper() + '_' + j.upper()
 
-    js = json.load(open("test_json.pkl"))
-    transformed_js = get_agg_face_attrs(js)
-    log.debug(str(transformed_js))
+                os.environ[env_var_name] = str(conf[i][j])
+                if return_values:
+                    vlist.append('{}="{}"'.format(env_var_name, conf[i][j]) )
+                else:
+                    vlist.append(env_var_name)
+    return vlist
+
+def run_docker(img_name = "vykhand/aa-backend:latest"):
+    ''' reading env vars and runs docker file '''
+    env_vars = set_env()
+
+    #constructing a list of env vars to pass to command line
+    var_string = tuple(chain(zip(["-e",]*len(env_vars),env_vars)))
+
+    try:
+        PORT = int(os.environ.get('APP_SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+
+    cmd = ["docker",  "run", "-p", "{}:{}".format(PORT,PORT)]
+    cmd += var_string
+    cmd.append(conf['web_app'].get('container_name', img_name))
+
+    #log.debug("running command {}".format(" ".join(cmd)))
+    log.debug("running command " + str(cmd))
+
+    subprocess.Popen(cmd)
+
+
+def get_create_plan_cmd(settings):
+    cmd = ("az appservice plan create --sku {sku} --is-linux -l " +
+                "{region} -n {plan_name} -g {resource_group}").format(**settings)
+
+    log.debug("create plan cmd :\n" + cmd )
+    return cmd
+
+def get_webapp_cmd(settings):
+    cmd = ("az webapp create -n {app_name} -g {resource_group} " +
+                "--plan {plan_name} -i {container_name}").format(**settings)
+
+    log.debug("create app cmd :\n" + cmd )
+    return cmd
+
+def get_settings_cmd(settings):
+    cmd = ("az webapp config appsettings set -g {resource_group} " +
+                "-n {app_name} --settings ").format(**settings)
+    env_vars = set_env(return_values=True)
+    cmd += " ".join(env_vars)
+
+    log.debug("Set app settings cmd :\n" + cmd )
+    return cmd
+
+def get_restart_cmd(settings):
+    cmd = ("az webapp restart -g {resource_group} -n {app_name}").format(**settings)
+
+    log.debug("Restart app cmd :\n" + cmd )
+    return cmd
+
+def delete_webapp():
+    settings = conf['web_app']
+
+    cmd =  "az webapp delete -n {app_name} -g {resource_group}".format(**settings)
+    log.debug("delete app command: \n" + cmd)
+    subprocess.call(cmd,shell=True)
+
+def create_webapp(dry_run = False):
+    '''
+    A helper routine that either runs or prints az commands to install your web app.
+    Settings for web app must be configured in config.yml "app" section.
+    You need to first run az login and az account set to use it
+    '''
+
+    settings = conf['web_app']
+
+    commands = []
+    commands.append(get_create_plan_cmd(settings))
+    commands.append(get_webapp_cmd(settings))
+    commands.append(get_settings_cmd(settings))
+    commands.append(get_restart_cmd(settings))
+
+    for cmd in commands:
+        if dry_run:
+            print(cmd)
+        else:
+            subprocess.call(cmd, shell=True)
+
+
+
+if __name__ == "__main__":
+    log.setLevel(logging.getLevelName(get_setting("app","log_level")))
+    log.addHandler(logging.StreamHandler(stream = sys.stdout))
+    if sys.argv[1] == "run_docker":
+        run_docker()
+    elif sys.argv[1] == "create_app":
+        dry = len(sys.argv)>2 and (sys.argv[2] in ("-d" , "--dry"))
+        create_webapp(dry_run=dry)
+    elif sys.argv[1] == "delete_app":
+        delete_webapp()
+    elif sys.argv[1] == "setenv":
+        set_env()
+    else:
+        print("Usage: python util.py run_docker|create_app|delete_app|setenv [-d|--dry]")
 
 
 
